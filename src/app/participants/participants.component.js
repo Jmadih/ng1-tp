63612a3842d88369aa3a50c09efd502696a60257
jmadih <madih.jaafar@gmail.com>
2018-02-18 00:08:58 +0100

    step-02

--- src/app/participants/participants.component.js
+++ src/app/participants/participants.component.js
@@ -6,12 +6,12 @@
     .component('participantsComponent', {
       templateUrl: 'app/participants/participants.html',
       controller: participantsController,
-      controllerAs: 'pCtrl'
+      controllerAs: 'participantCtrl'
     });
 
   function participantsController() {
     var vm = this;
-    vm.rowCollection = [
+    vm.participants = [
       {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), email: 'whatever@gmail.com'},
       {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), email: 'oufblandou@gmail.com'},
       {firstName: 'Francoise', lastName: 'Duchess', birthDate: new Date('1955-08-27'), email: 'raymondef@gmail.com'}
