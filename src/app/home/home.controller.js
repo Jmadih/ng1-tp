8dfe2097ef944c4d0d076612fa18c68b7f1c08c0
jmadih <madih.jaafar@gmail.com>
2018-02-17 08:29:47 +0100

    fix data step-01

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -6,9 +6,9 @@
   function HomeController() {
     var vm = this;
     vm.rowCollection = [
-      {firstName: 'Laurentccc', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-      {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-      {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+      {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), email: 'whatever@gmail.com'},
+      {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), email: 'oufblandou@gmail.com'},
+      {firstName: 'Francoise', lastName: 'Duchess', birthDate: new Date('1955-08-27'), email: 'raymondef@gmail.com'}
     ];
   }
 })();
